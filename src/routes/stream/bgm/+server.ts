d187f3d7ce4d08571c612b55d898fbc6018a7797
Kiizuha <kiizuha@gnuweeb.org>
2024-02-19 16:22:33 +0700

    trying to fix no such file
    Signed-off-by: Kiizuha <kiizuha@gnuweeb.org>

--- src/routes/stream/bgm/+server.ts
+++ src/routes/stream/bgm/+server.ts
@@ -1,9 +1,7 @@
 import { read } from "$app/server";
-import { fileStreamResponse } from "$lib/functions";
 import type { RequestHandler } from "@sveltejs/kit";
 
 
-export const GET = (async ({ request }) => {
-  const fileRead = read("static/audio/constant-moderato.mp3");
-  return fileStreamResponse(request, await fileRead.text(), "audio/mpeg");
+export const GET = (async () => {
+  return read("/static/audio/constant-moderato.mp3");
 }) satisfies RequestHandler;
--- src/routes/stream/l2d/+server.ts
+++ src/routes/stream/l2d/+server.ts
@@ -1,9 +1,7 @@
 import { read } from "$app/server";
-import { fileStreamResponse } from "$lib/functions";
 import type { RequestHandler } from "@sveltejs/kit";
 
 
-export const GET = (async ({ request }) => {
-  const fileRead = read("static/video/yuuka-sports.webm");
-  return fileStreamResponse(request, await fileRead.text() , "video/webm");
+export const GET = (async () => {
+  return read("/static/video/yuuka-sports.webm");
 }) satisfies RequestHandler;
